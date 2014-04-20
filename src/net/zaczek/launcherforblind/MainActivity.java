7c0ca532d29333c60717012948466d78052b043e
Arthur Zaczek <arthur@dasz.at>
2014-04-20 20:27:44 +0200

    initial commit

---
+++ src/net/zaczek/launcherforblind/MainActivity.java
@@ -0,0 +1,12 @@
+package net.zaczek.launcherforblind;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
