9e52d5f4f7c5a1d00afca7c9ba190e011ffffc87
Arthur Zaczek <arthur@dasz.at>
2014-04-21 21:57:12 +0200

    Announcing time & date

--- src/net/zaczek/launcherforblind/TimeListEntry.java
+++ src/net/zaczek/launcherforblind/TimeListEntry.java
@@ -1,6 +1,13 @@
 package net.zaczek.launcherforblind;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class TimeListEntry extends AbstractListEntry {
+
+	final Calendar cal = Calendar.getInstance();
+	final SimpleDateFormat monthFormat = new SimpleDateFormat("MMMM", Locale.getDefault());
 
 	public TimeListEntry(String label) {
 		super(label);
@@ -12,6 +19,13 @@ public class TimeListEntry extends AbstractListEntry {
 
 	@Override
 	public String getTextToSay() {
-		return "Es ist schon spät";
+		final int day = cal.get(Calendar.DAY_OF_MONTH);
+		final String monthStr = monthFormat.format(cal.getTime());
+		final int hour = cal.get(Calendar.HOUR_OF_DAY);
+		final int minutes = cal.get(Calendar.MINUTE);
+
+		return "Es ist " + Integer.toString(hour) + " Uhr "
+				+ Integer.toString(minutes) + " am " + Integer.toString(day)
+				+ " " + monthStr;
 	}
 }
